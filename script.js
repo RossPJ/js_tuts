1f7eeca21607ca6ec076768e2aa007931cba34ab
Patrick Ross <pross17@wgu.edu>
2024-06-20 20:36:31 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,73 @@
+//There are two gymnastics teams: Dolphins and Koalas. 
+//They compete against each other 3 times. 
+//The winner with the highest average score wins a trophy
+
+// var scoreDolphins = (96 + 108 + 89) / 3; //average dolphins score 
+// var scoreKoolas = (88 + 100 + 110) / 3; //average koolas score
+
+// if (scoreDolphins > scoreKoolas) {
+//     console.log('Dolphins win the trophy')
+// } else if (scoreDolphins < scoreKoolas) {
+//     console.log('Koolas win the trophy')
+// } else {
+//     console.log('Both win the trophy')
+// }
+
+//_______________________________________________________________
+
+//switch case example 
+// const day = 'wednesday';
+// switch(day) {
+//     case 'monday':
+//         console.log('plan course structure');
+//         console.log('plan course something');
+//         break;
+//     case 'tuesday':
+//         console.log('plan course structure');
+//         break;
+//     case 'wednesday':
+//         console.log('plan course structure');
+//         break;
+//     case 'thursday':
+//     case 'friday':
+//         console.log('know idea what im doing')
+//         break;
+//     default:
+//         console.log('not a valid day')
+//     }
+
+//________________________________________________________________
+
+//template literall
+
+// var new_list = ['tom', 'pills', 23, 'city bus']
+// var new_str = `This guy named ${new_list[0]} came around one day and was looking for some ${new_list[1]}`
+// console.log(new_str)
+
+
+//_______________________________________________________________
+
+//ternary / conditional operator 
+
+// var age = 23; 
+
+// //condtion ? if statement : else statement 
+// age >= 18 ? console.log('i like to drink cheese') : console.log('I like to eat water');
+
+
+body{
+    font-family:Verdana,Arial,sans-serif;
+    font-size:8pt;
+    /* [ReplaceColor(themeColor:"Dark2-Darker")] */ color:#676767;
+    /* [ReplaceColor(themeColor:"Light1")] */ background-color:#fff;
+    margin:0px;
+    padding:0px;
+    
+    }
+
+    body.v4master{
+        height:100%;
+        width:100%;
+        overflow:hidden;
+        }
+        
